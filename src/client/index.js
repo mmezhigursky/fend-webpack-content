e939e276f30dbb538b071f6a38a97b15293cb974
Max <m.mezhigurskii@gmail.com>
2020-05-30 22:02:12 +0300

    babel init

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,6 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+
+console.log(checkForName);
 alert("I am exist")
+//npm i -D @babel/core @babel/preset-env babel-loader
