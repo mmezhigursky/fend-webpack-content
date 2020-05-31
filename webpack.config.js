4d76d9a0a2d7b34055256c2fc71ffa3d86eb7f7e
Max <m.mezhigurskii@gmail.com>
2020-05-31 11:40:14 +0300

    install html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path') 
 const webpack = require('webpack')
+const HtmlWebPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -11,5 +12,12 @@ module.exports = {
             loader: "babel-loader"
                 }
         ]
-}
+},
+
+    plugins: [
+        new  HtmlWebPlugin({
+        template: "./src/client/views/index.html",
+        filename: "./index.html"
+        })
+    ]
 }
